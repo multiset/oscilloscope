eaf069841a6b8534c4d59dcc7bff4df5bd9ff568
Benjamin Anderson <b@banjiewen.net>
2013-09-30 22:24:35 -0700

    Update comment on compression

--- apps/oscilloscope_cache/src/oscilloscope_cache.erl
+++ apps/oscilloscope_cache/src/oscilloscope_cache.erl
@@ -28,7 +28,7 @@
 %% Minimim amount of seconds to wait before persisting to disk
 -define(MIN_PERSIST_AGE, 300).
 
-%% TODO: compress
+%% TODO: try zlib:zip instead
 -define(VALENCODE(V), zlib:compress(term_to_binary(V))).
 -define(VALDECODE(V), binary_to_term(zlib:decompress(V))).
 
