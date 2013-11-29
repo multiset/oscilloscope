c3b0c0e9597ae5bc9fafc2a10870b1505473d935
Benjamin Anderson <b@banjiewen.net>
2013-11-29 12:32:02 -0800

    Move cache macros to dedicated include file

--- apps/oscilloscope_cache/src/oscilloscope_cache.erl
+++ apps/oscilloscope_cache/src/oscilloscope_cache.erl
@@ -18,6 +18,7 @@
 ]).
 
 -include_lib("oscilloscope/include/oscilloscope_types.hrl").
+-include("oscilloscope_cache.hrl").
 
 -record(st, {
     resolution_id :: resolution_id(),
@@ -28,24 +29,6 @@
     aggregation_fun :: fun(),
     commutator
 }).
-
-%% Minimim amount of seconds to wait before persisting to disk
--define(MIN_PERSIST_AGE, 300).
-
-%% TODO: try zlib:zip instead
--define(VALENCODE(V), zlib:compress(term_to_binary(V))).
--define(VALDECODE(V), binary_to_term(zlib:uncompress(V))).
-
-%% Number of bytes to store in each value
--define(MIN_CHUNK_SIZE, 1000).
--define(MAX_CHUNK_SIZE, 1024).
-
-%% Dynamo/commutator things. Should go in config
--define(DYNAMO_TABLE, <<"oscilloscope-test">>).
--define(DYNAMO_SCHEMA, [{<<"id">>, n}, {<<"t">>, n}, {<<"v">>, b}]).
--define(DYNAMO_REGION, "us-east-1").
--define(DYNAMO_ACCESSKEY, "AKIAJNCK2CQXCEEM6MDA").
--define(DYNAMO_SECRETKEY, "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").
 
 start() ->
     application:start(oscilloscope_cache).
