fce5a094094f2791ffdd5a3ff61f074036bb0a56
Benjamin Bastian <benjamin.bastian@gmail.com>
2014-01-08 19:27:54 -0800

    Numerous updates to Postgres schema
    1) Remove auto-generated comments
    2) Add users table
    3) Update IDs in various tables to be primary keys
    4) Add UNIQUE constraints to various tables
    5) Add preliminary table for shared metrics

--- apps/oscilloscope_sql/priv/schema.sql
+++ apps/oscilloscope_sql/priv/schema.sql
@@ -1,26 +1,10 @@
---
--- PostgreSQL database dump
---
-
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
 CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
 
 SET search_path = public, pg_catalog;
 
@@ -28,237 +12,49 @@ SET default_tablespace = '';
 
 SET default_with_oids = false;
 
---
--- Name: metrics; Type: TABLE; Schema: public; Owner: benjamin; Tablespace: 
---
 
-CREATE TABLE metrics (
-    id integer NOT NULL,
-    username integer,
-    name bytea NOT NULL,
-    host bytea NOT NULL,
-    aggregation bytea NOT NULL
-);
+CREATE TYPE metric_perms AS ENUM ('r', 'w', 'rw');
 
-
-ALTER TABLE public.metrics OWNER TO benjamin;
-
---
--- Name: metrics_id_seq; Type: SEQUENCE; Schema: public; Owner: benjamin
---
-
-CREATE SEQUENCE metrics_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.metrics_id_seq OWNER TO benjamin;
-
---
--- Name: metrics_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: benjamin
---
-
-ALTER SEQUENCE metrics_id_seq OWNED BY metrics.id;
-
-
---
--- Name: metrics_id_seq; Type: SEQUENCE SET; Schema: public; Owner: benjamin
---
-
-SELECT pg_catalog.setval('metrics_id_seq', 1, false);
-
-
---
--- Name: resolutions; Type: TABLE; Schema: public; Owner: benjamin; Tablespace: 
---
-
-CREATE TABLE resolutions (
-    id integer NOT NULL,
-    metric_id integer,
-    "interval" integer,
-    count integer,
-    persisted integer[]
-);
-
-
-ALTER TABLE public.resolutions OWNER TO benjamin;
-
---
--- Name: resolutions_id_seq; Type: SEQUENCE; Schema: public; Owner: benjamin
---
-
-CREATE SEQUENCE resolutions_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.resolutions_id_seq OWNER TO benjamin;
-
---
--- Name: resolutions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: benjamin
---
-
-ALTER SEQUENCE resolutions_id_seq OWNED BY resolutions.id;
-
-
---
--- Name: resolutions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: benjamin
---
-
-SELECT pg_catalog.setval('resolutions_id_seq', 1, false);
-
-
---
--- Name: users; Type: TABLE; Schema: public; Owner: benjamin; Tablespace: 
---
 
 CREATE TABLE users (
-    id integer NOT NULL,
-    username bytea NOT NULL
+    id serial PRIMARY KEY,
+    username bytea UNIQUE NOT NULL,
+    port integer UNIQUE NOT NULL,
+    email bytea NOT NULL,
+    password bytea NOT NULL
+);
+
+CREATE TABLE metrics (
+    id serial PRIMARY KEY,
+    user_id integer NOT NULL,
+    name bytea NOT NULL,
+    host bytea NOT NULL,
+    aggregation bytea NOT NULL,
+    UNIQUE(user_id, name, host, aggregation)
+);
+
+CREATE TABLE resolutions (
+    id serial PRIMARY KEY,
+    metric_id integer NOT NULL,
+    "interval" integer,
+    count integer,
+    persisted integer[],
+    UNIQUE(metric_id, "interval", count, persisted)
+);
+
+CREATE TABLE shared_metrics (
+    metric_id serial REFERENCES metrics(id),
+    user_id serial REFERENCES users(id),
+    perms metric_perms
 );
 
 
-ALTER TABLE public.users OWNER TO benjamin;
+CREATE INDEX sm_metric_id_idx ON shared_metrics (metric_id);
 
---
--- Name: users_id_seq; Type: SEQUENCE; Schema: public; Owner: benjamin
---
-
-CREATE SEQUENCE users_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
+CREATE INDEX sm_user_id_idx ON shared_metrics (user_id);
 
 
-ALTER TABLE public.users_id_seq OWNER TO benjamin;
-
---
--- Name: users_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: benjamin
---
-
-ALTER SEQUENCE users_id_seq OWNED BY users.id;
-
-
---
--- Name: users_id_seq; Type: SEQUENCE SET; Schema: public; Owner: benjamin
---
-
-SELECT pg_catalog.setval('users_id_seq', 1, true);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: benjamin
---
-
-ALTER TABLE ONLY metrics ALTER COLUMN id SET DEFAULT nextval('metrics_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: benjamin
---
-
-ALTER TABLE ONLY resolutions ALTER COLUMN id SET DEFAULT nextval('resolutions_id_seq'::regclass);
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: benjamin
---
-
-ALTER TABLE ONLY users ALTER COLUMN id SET DEFAULT nextval('users_id_seq'::regclass);
-
-
---
--- Data for Name: metrics; Type: TABLE DATA; Schema: public; Owner: benjamin
---
-
-COPY metrics (id, username, name, host, aggregation) FROM stdin;
-\.
-
-
---
--- Data for Name: resolutions; Type: TABLE DATA; Schema: public; Owner: benjamin
---
-
-COPY resolutions (id, metric_id, "interval", count, persisted) FROM stdin;
-\.
-
-
---
--- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: benjamin
---
-
-COPY users (id, username) FROM stdin;
-1	\\x62616e6a696577656e
-\.
-
-
---
--- Name: metrics_pkey; Type: CONSTRAINT; Schema: public; Owner: benjamin; Tablespace: 
---
-
-ALTER TABLE ONLY metrics
-    ADD CONSTRAINT metrics_pkey PRIMARY KEY (id);
-
-
---
--- Name: metrics_username_name_host_key; Type: CONSTRAINT; Schema: public; Owner: benjamin; Tablespace: 
---
-
-ALTER TABLE ONLY metrics
-    ADD CONSTRAINT metrics_username_name_host_key UNIQUE (username, name, host);
-
-
---
--- Name: resolutions_pkey; Type: CONSTRAINT; Schema: public; Owner: benjamin; Tablespace: 
---
-
-ALTER TABLE ONLY resolutions
-    ADD CONSTRAINT resolutions_pkey PRIMARY KEY (id);
-
-
---
--- Name: users_pkey; Type: CONSTRAINT; Schema: public; Owner: benjamin; Tablespace: 
---
-
-ALTER TABLE ONLY users
-    ADD CONSTRAINT users_pkey PRIMARY KEY (id);
-
-
---
--- Name: metrics_username_fkey; Type: FK CONSTRAINT; Schema: public; Owner: benjamin
---
-
-ALTER TABLE ONLY metrics
-    ADD CONSTRAINT metrics_username_fkey FOREIGN KEY (username) REFERENCES users(id);
-
-
---
--- Name: resolutions_metric_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: benjamin
---
-
-ALTER TABLE ONLY resolutions
-    ADD CONSTRAINT resolutions_metric_id_fkey FOREIGN KEY (metric_id) REFERENCES metrics(id);
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: benjamin
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM benjamin;
-GRANT ALL ON SCHEMA public TO benjamin;
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
-
+ALTER TABLE public.metrics OWNER TO oscilloscope;
+ALTER TABLE public.resolutions OWNER TO oscilloscope;
+ALTER TABLE public.users OWNER TO oscilloscope;
+ALTER TABLE public.shared_metrics OWNER TO oscilloscope;
