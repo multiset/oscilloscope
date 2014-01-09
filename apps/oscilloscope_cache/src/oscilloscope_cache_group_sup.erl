ecbcd32a17d17d7cfc29d7b5419f18dc815c71eb
Benjamin Anderson <b@banjiewen.net>
2014-01-08 20:25:29 -0800

    Add basic metric configuration functionality to oscilloscope_cache

--- apps/oscilloscope_cache/src/oscilloscope_cache_group_sup.erl
+++ apps/oscilloscope_cache/src/oscilloscope_cache_group_sup.erl
@@ -14,14 +14,14 @@ init({UserID, Name, Host}=Group) ->
         {oscilloscope_cache, group_spawns},
         {inc, 1}
     ),
-    {AF, Resolutions} = get_or_create_resolutions(UserID, Name, Host),
+    {AF, Resolutions} = get_or_create_group_configuration(UserID, Name, Host),
     Specs = lists:map(fun(R) -> generate_spec(Group, AF, R) end, Resolutions),
     {ok, {{one_for_all, 10, 10}, Specs}}.
 
--spec get_or_create_resolutions(binary(), binary(), binary()) ->
+-spec get_or_create_group_configuration(binary(), binary(), binary()) ->
     {atom(), [resolution()]}.
-get_or_create_resolutions(UserID, Name, Host) ->
-    case oscilloscope_sql_metrics:get(User, Name, Host) of
+get_or_create_group_configuration(UserID, Name, Host) ->
+    case oscilloscope_sql_metrics:get(UserID, Name, Host) of
         {ok, Values} ->
             Values;
         {error, not_found} ->
@@ -29,13 +29,13 @@ get_or_create_resolutions(UserID, Name, Host) ->
                 {oscilloscope_cache, group_creations},
                 {inc, 1}
             ),
-            {AggregationFun, Resolutions} = get_metric_configuration(
+            {AggregationFun, Resolutions} = build_group_configuration(
                 UserID, Name, Host
             ),
             ok = oscilloscope_sql_metrics:create(
                 UserID, Name, Host, AggregationFun, Resolutions
             ),
-            get_or_create_resolutions(UserID, Name, Host)
+            get_or_create_group_configuration(UserID, Name, Host)
     end.
 
 -spec generate_spec(group(), aggregation(), resolution()) -> child_spec().
@@ -82,14 +82,58 @@ generate_spec(Group, AggregationFun, {Id, Interval, Count, Persisted}=Res) ->
         permanent, 5000, worker, [oscilloscope_cache]
     }.
 
-get_metric_configuration(_UserID, _Name, _Host) ->
-    %% TODO: get from database
+build_group_configuration(UserID, Service, Host) ->
+    {ok, AggConfigs} = oscilloscope_sql_metrics:get_aggregation_configuration(
+        UserID
+    ),
+    {ok, ResConfigs} = oscilloscope_sql_metrics:get_resolution_configuration(
+        UserID
+    ),
+    AggregationFun = case find_config_match(Service, Host, AggConfigs) of
+        nomatch -> get_default_aggregation_fun();
+        AggMatch -> parse_aggregation_fun_match(AggMatch)
+    end,
+    Resolutions = case find_config_match(Service, Host, ResConfigs) of
+        nomatch -> get_default_resolutions();
+        ResMatch -> parse_resolution_match(ResMatch)
+    end,
+    %% TODO: consider removing persisted points from resolutions
+    Resolutions1 = lists:map(fun({I, C}) -> {I, C, []} end, Resolutions),
+    {AggregationFun, Resolutions1}.
+
+find_config_match(_Service, _Host, []) ->
+    nomatch;
+find_config_match(Service, Host, [{ServiceRegex, HostRegex, Config}|Cs]) ->
+    ServiceMatch = re:run(Service, ServiceRegex) =/= nomatch,
+    HostMatch = re:run(Host, HostRegex) =/= nomatch,
+    case ServiceMatch and HostMatch of
+        true -> Config;
+        false -> find_config_match(Service, Host, Cs)
+    end.
+
+get_default_aggregation_fun() ->
     {ok, AggregationFun} = application:get_env(
         oscilloscope_cache,
         default_aggregation_fun
     ),
+    AggregationFun.
+
+get_default_resolutions() ->
     {ok, Resolutions} = application:get_env(
         oscilloscope_cache,
         default_resolutions
     ),
-    {AggregationFun, Resolutions}.
+    Resolutions.
+
+parse_aggregation_fun_match(<<"avg">>) ->
+    avg;
+parse_aggregation_fun_match(_) ->
+    get_default_aggregation_fun().
+
+parse_resolution_match(Match) ->
+    case oscilloscope_cache_util:parse_resolution(Match) of
+        {error, _, _} ->
+            get_default_resolutions();
+        Resolutions ->
+            Resolutions
+    end.
