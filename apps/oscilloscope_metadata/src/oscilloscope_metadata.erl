41addba35fec064a9415ac3b641f7711e6b108b2
Benjamin Anderson <b@banjiewen.net>
2014-05-03 16:19:57 -0700

    Various fixes to oscilloscope_metadata

--- apps/oscilloscope_metadata/src/oscilloscope_metadata.erl
+++ apps/oscilloscope_metadata/src/oscilloscope_metadata.erl
@@ -1,9 +1,23 @@
 -module(oscilloscope_metadata).
 
 -export([start/0, stop/0]).
+-export([adhoc/2, named/2]).
 
 start() ->
     application:start(oscilloscope_metadata).
 
 stop() ->
     application:stop(oscilloscope_metadata).
+
+adhoc(SQL, Fields) ->
+    folsom_metrics:notify({oscilloscope_metadata, adhoc_queries}, {inc, 1}),
+    transact({adhoc, SQL, Fields}).
+
+named(Smt, Fields) ->
+    folsom_metrics:notify({oscilloscope_metadata, named_queries}, {inc, 1}),
+    transact({named, Smt, Fields}).
+
+transact(Msg) ->
+    poolboy:transaction(
+      pgsql,
+      fun(Worker) -> gen_server:call(Worker, Msg) end).
--- apps/oscilloscope_metadata/src/oscilloscope_metadata_sup.erl
+++ apps/oscilloscope_metadata/src/oscilloscope_metadata_sup.erl
@@ -1,9 +1,7 @@
 -module(oscilloscope_metadata_sup).
 -behaviour(supervisor).
 
--export([start_link/0]).
-
--export([init/1]).
+-export([start_link/0, init/1]).
 
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
