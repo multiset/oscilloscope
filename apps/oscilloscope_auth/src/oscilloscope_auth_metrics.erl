93567a8eea88fe965a771b7fa5c456c7ef491b3f
Benjamin Bastian <benjamin.bastian@gmail.com>
2014-06-17 23:52:51 -0700

    change osc_auth_metrics:get_metrics to o_a_m:get because I like it better

--- apps/oscilloscope_auth/src/oscilloscope_auth_metrics.erl
+++ apps/oscilloscope_auth/src/oscilloscope_auth_metrics.erl
@@ -1,10 +1,10 @@
 -module(oscilloscope_auth_metrics).
 
 -export([
-    get_metrics/3
+    get/3
 ]).
 
-get_metrics(OrgID, UserID, Tags) ->
+get(OrgID, UserID, Tags) ->
     gen_server:call(
         oscilloscope_auth_server,
         {get_metrics, OrgID, UserID, Tags}
