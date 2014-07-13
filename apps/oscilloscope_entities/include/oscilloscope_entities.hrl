b8c763af82c6292f41aa1519f27adc4d141e15be
Benjamin Bastian <benjamin.bastian@gmail.com>
2014-07-12 22:51:09 -0700

    Fix file name

---
+++ apps/oscilloscope_entities/include/oscilloscope_entities.hrl
@@ -0,0 +1,21 @@
+-record(user, {
+    id :: integer(),
+    owner_id :: integer(),
+    password :: binary(),
+    name :: binary(),
+    email :: binary(),
+    created_at :: integer(),
+    updated_at :: integer()
+}).
+
+-record(org, {
+    id :: integer(),
+    owner_id :: integer(),
+    name :: binary()
+}).
+
+-record(team, {
+    id :: integer(),
+    org_id :: integer(),
+    name :: binary()
+}).
