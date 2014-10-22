8ae43d0b7670f8811a6e42c173254fae638f5296
Benjamin Bastian <benjamin.bastian@gmail.com>
2014-10-21 18:19:53 -0700

    Various bug fixes to make osc_meta_user work

--- apps/osc_meta/src/osc_meta_user.erl
+++ apps/osc_meta/src/osc_meta_user.erl
@@ -1,6 +1,7 @@
 -module(osc_meta_user).
 
 -export([
+    lookup/1,
     create/2,
     delete/1,
     teams/2,
@@ -12,34 +13,63 @@
 
 -include_lib("osc/include/osc_types.hrl").
 
--spec hash_password(binary()) -> binary().
-hash_password(Pass) ->
-    {ok, Salt} = bcrypt:gen_salt(),
-    {ok, LHash} = bcrypt:hashpw(Pass, Salt),
-    list_to_binary(LHash).
+-spec lookup(binary() | user_id()) -> {ok, [{any(), any()}]} | not_found.
+lookup(NameOrID) ->
+    [{ok, _, UserLookup}, {ok, _, Emails}] = if is_integer(NameOrID) ->
+        osc_sql:batch([
+            {get_user_by_id, [NameOrID]},
+            {get_emails_by_id, [NameOrID]}
+        ]);
+    true ->
+        osc_sql:batch([
+            {get_user_by_name, [NameOrID]},
+            {get_emails_by_name, [NameOrID]}
+        ])
+    end,
+    case UserLookup of
+        [] ->
+            not_found;
+        [{UserID, OwnerID, Name, Pass, _Active}] ->
+            {ok, [
+                {id, UserID},
+                {owner_id, OwnerID},
+                {name, Name},
+                {password, Pass},
+                {emails, Emails}
+            ]}
+    end.
 
 -spec create(binary(), binary()) -> {ok, user_id()}.
 create(Name, Pass) ->
     Hash = hash_password(Pass),
-    {ok,1,_,[{User}]} = osc_sql:named(create_user, [Name, Hash]),
-    {ok, User}.
+    {ok,1,_,[{UserID}]} = osc_sql:named(create_user, [Name, Hash]),
+    {ok, UserID}.
+
+-spec delete(user_id()) -> ok.
+delete(UserID) ->
+    [{ok,_},{ok,1},{ok,_},{ok,_}] = osc_sql:batch([
+        {delete_emails, [UserID]},
+        {delete_user, [UserID]},
+        {remove_user_from_all_orgs, [UserID]},
+        {remove_user_from_all_teams, [UserID]}
+    ]),
+    ok.
 
 -spec add_email(user_id(), binary()) -> ok.
 add_email(UserID, Email) ->
-    {ok,1,_} = osc_sql:named(add_email, [UserID, Email]).
+    {ok,1} = osc_sql:named(add_email, [UserID, Email]),
+    ok.
 
 -spec remove_email(user_id(), binary()) -> ok.
 remove_email(UserID, Email) ->
-    {ok,_} = osc_sql:named(remove_email, [UserID, Email]).
+    {ok,1} = osc_sql:named(remove_email, [UserID, Email]),
+    ok.
 
 -spec change_password(user_id(), binary()) -> ok.
 change_password(UserID, NewPass) ->
     Hash = hash_password(NewPass),
-    {ok,_} = osc_sql:named(change_password, [UserID, Hash]).
-
--spec delete(user_id()) -> ok.
-delete(UserID) ->
-    {ok,_} = osc_sql:named(delete_user, [UserID]).
+    {ok,_} = osc_sql:named(change_password, [UserID, Hash]),
+    ok.
 
 -spec teams(org_id(), user_id()) -> [team_id()].
 teams(OrgID, UserID) ->
@@ -48,3 +78,9 @@ teams(OrgID, UserID) ->
 -spec orgs(user_id()) -> [org_id()].
 orgs(UserID) ->
     {ok,_} = osc_sql:named(get_user_orgs, [UserID]).
+
+-spec hash_password(binary()) -> binary().
+hash_password(Pass) ->
+    {ok, Salt} = bcrypt:gen_salt(),
+    {ok, LHash} = bcrypt:hashpw(Pass, Salt),
+    list_to_binary(LHash).
