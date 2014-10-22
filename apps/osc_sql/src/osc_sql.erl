1c071ad05fdfdfc01dcbe0e7b5b5117565beb3f5
Benjamin Bastian <benjamin.bastian@gmail.com>
2014-10-21 18:09:03 -0700

    Fix osc_sql name

--- apps/osc_sql/src/osc_sql.erl
+++ apps/osc_sql/src/osc_sql.erl
@@ -1,6 +1,6 @@
--module(osc_sql_sql).
+-module(osc_sql).
 
--export([adhoc/2, named/2]).
+-export([adhoc/2, named/2, batch/1]).
 
 adhoc(SQL, Fields) ->
     folsom_metrics:notify({osc_sql, adhoc_queries}, {inc, 1}),
@@ -9,6 +9,10 @@ adhoc(SQL, Fields) ->
 named(Smt, Fields) ->
     folsom_metrics:notify({osc_sql, named_queries}, {inc, 1}),
     transact({named, Smt, Fields}).
+
+batch(SmtFields) ->
+    folsom_metrics:notify({osc_sql, batch_queries}, {inc, 1}),
+    transact({batch, SmtFields}).
 
 transact(Msg) ->
     poolboy:transaction(
