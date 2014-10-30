f452cbba29a35dd5907dfd2e9539a796b68626e7
Benjamin Anderson <b@banjiewen.net>
2014-10-29 20:56:37 -0700

    Add statement reload feature to osc_sql

--- apps/osc_sql/src/osc_sql.erl
+++ apps/osc_sql/src/osc_sql.erl
@@ -1,6 +1,6 @@
 -module(osc_sql).
 
--export([adhoc/2, named/2, batch/1]).
+-export([adhoc/2, named/2, batch/1, reload_statements/0]).
 
 adhoc(SQL, Fields) ->
     folsom_metrics:notify({osc_sql, adhoc_queries}, {inc, 1}),
@@ -13,6 +13,12 @@ named(Smt, Fields) ->
 batch(SmtFields) ->
     folsom_metrics:notify({osc_sql, batch_queries}, {inc, 1}),
     transact({batch, SmtFields}).
+
+reload_statements() ->
+    Workers = poolboy:get_all_workers(pgsql),
+    lists:foreach(fun(W) ->
+        ok = gen_server:call(W, reload_statements)
+    end, Workers).
 
 transact(Msg) ->
     poolboy:transaction(
