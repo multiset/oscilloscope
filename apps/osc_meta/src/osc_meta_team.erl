d03c44ca8ff8421ab722e94d3caa143bc784ec58
Benjamin Anderson <b@banjiewen.net>
2014-11-07 22:23:05 -0800

    Add team resource

--- apps/osc_meta/src/osc_meta_team.erl
+++ apps/osc_meta/src/osc_meta_team.erl
@@ -1,6 +1,8 @@
 -module(osc_meta_team).
 
 -export([
+    lookup/1,
+    lookup_members/1,
     create/2,
     delete/2,
     members/2,
@@ -10,6 +12,38 @@
 ]).
 
 -include_lib("osc/include/osc_types.hrl").
+
+-spec lookup(team_id()) -> not_found | {ok, list()}.
+lookup(TeamID) ->
+    SQL = <<
+        "SELECT teams.name, teams.org_id"
+        " FROM teams"
+        " WHERE teams.id=$1"
+    >>,
+    {ok, _Schema, Rows} = osc_sql:adhoc(SQL, [TeamID]),
+    case Rows of
+        [] ->
+            not_found;
+        [{TeamName, OrgID}] ->
+            {ok, Members} = lookup_members(TeamID),
+            {ok, [
+                {id, TeamID},
+                {name, TeamName},
+                {orgid, OrgID},
+                {members, Members}
+            ]}
+    end.
+
+-spec lookup_members(team_id()) -> {ok, list()}.
+lookup_members(TeamID) ->
+    SQL = <<
+        "SELECT users.id, users.name"
+        " FROM users"
+        " JOIN team_members on users.id=team_members.user_id"
+        " WHERE team_members.team_id=$1"
+    >>,
+    {ok, _Schema, Rows} = osc_sql:adhoc(SQL, [TeamID]),
+    {ok, Rows}.
 
 -spec create(org_id(), binary()) -> {ok, team_id()}.
 create(OrgID, Name) ->
