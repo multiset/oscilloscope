6d82c87222d04d116b27848c986db11d5b8ecf3f
Benjamin Anderson <b@banjiewen.net>
2014-12-11 16:08:17 -0800

    Add auth to osc_meta_org_handler

--- apps/osc_meta/src/osc_meta_org_handler.erl
+++ apps/osc_meta/src/osc_meta_org_handler.erl
@@ -4,6 +4,8 @@
 -export([
     rest_init/2,
     allowed_methods/2,
+    is_authorized/2,
+    forbidden/2,
     resource_exists/2,
     content_types_accepted/2,
     content_types_provided/2,
@@ -12,6 +14,7 @@
 ]).
 
 -record(st, {
+    user_id,
     org_props
 }).
 
@@ -21,26 +24,56 @@ init({tcp, http}, _Req, _Opts) ->
 terminate(_Reason, _Req, _State) ->
     ok.
 
-rest_init(Req, _State) ->
-    {ok, Req, #st{}}.
-
-allowed_methods(Req, State) ->
-    {[<<"GET">>, <<"PATCH">>], Req, State}.
-
-resource_exists(Req, State) ->
-    case cowboy_req:binding(orgid, Req) of
+rest_init(Req0, _) ->
+    State = #st{},
+    case cowboy_req:binding(org_id, Req0) of
         {undefined, Req1} ->
-            {false, Req1, State};
+            {ok, Req1, State};
         {OrgIDBin, Req1} ->
             OrgID = list_to_integer(binary_to_list(OrgIDBin)),
             case osc_meta_org:lookup(OrgID) of
                 not_found ->
-                    lager:error("Org not found"),
-                    {false, Req1, State};
+                    {ok, Req1, State};
                 {ok, OrgProps} ->
-                    {true, Req1, State#st{org_props=OrgProps}}
+                    {ok, Req1, State#st{org_props=OrgProps}}
             end
     end.
+
+allowed_methods(Req, State) ->
+    {[<<"GET">>, <<"PATCH">>], Req, State}.
+
+is_authorized(Req, State) ->
+    osc_http:is_authorized(
+        Req,
+        fun(UserID) -> State#st{user_id=UserID} end,
+        fun() -> State end
+    ).
+
+forbidden(Req0, State) ->
+    #st{org_props = OrgProps, user_id = UserID} = State,
+    case OrgProps of
+        undefined ->
+            {false, Req0, State};
+        OrgProps ->
+            OrgID = proplists:get_value(id, OrgProps),
+            {Method, Req1} = cowboy_req:method(Req0),
+            Forbidden = case Method of
+                <<"PATCH">> ->
+                    %% Mutation requires ownership
+                    IsOwner = osc_meta_org:is_owner(OrgID, UserID),
+                    not IsOwner;
+                <<"GET">> ->
+                    %% Viewing only requires membership
+                    IsMember = osc_meta_org:is_member(OrgID, UserID),
+                    not IsMember
+            end,
+            {Forbidden, Req1, State}
+    end.
+
+resource_exists(Req, #st{org_props=undefined}=State) ->
+    {false, Req, State};
+resource_exists(Req, State) ->
+    {true, Req, State}.
 
 content_types_accepted(Req, State) ->
     {
