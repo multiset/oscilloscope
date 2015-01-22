7e8020f71041d0711ff0282456428a5c1cf50f5e
Benjamin Anderson <b@banjiewen.net>
2015-01-22 10:47:24 -0800

    Instrument osc_meta

--- apps/osc_meta/src/osc_meta_sup.erl
+++ apps/osc_meta/src/osc_meta_sup.erl
@@ -7,5 +7,11 @@ start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
+    mstat:new_counter([osc_meta, creations, user]),
+    mstat:new_counter([osc_meta, creations, org]),
+    mstat:new_counter([osc_meta, creations, team]),
+    mstat:new_counter([osc_meta, creations, metric]),
+    mstat:new_counter([osc_meta, creations, window]),
+    mstat:new_counter([osc_meta, creations, window_configuration]),
     {ok, {{one_for_one, 5, 10}, []}}.
 
